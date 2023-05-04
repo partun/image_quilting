21513c369f0408fbe46cf99286fc75f7fc20150f
Dominic <steinedo@student.ethz.ch>
2023-05-04 17:23:44 +0200

    quilting algorithm fix

--- src/calc_errors.c
+++ src/calc_errors.c
@@ -1,64 +1,111 @@
 //
 // Created by Oleh Kuzyk on 10.03.23.
 //
-#include "load_image.h"
 #include "calc_errors.h"
-
+#include "image.h"
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include <float.h>
 
-double *calc_errors(Image *image, Image *out_slice, int block_size, int overlap_size, Direction direction) {
-    int image_height = image->height;
-    int image_width = image->width;
+/*
+ * computes the overlap error for all possible src image blocks
+ */
+double *
+calc_errors(Image *src, Image *out, ImageCoordinates out_coord, int block_size,
+            int overlap, Direction direction) {
 
-    int num_blocks_h = image_height - block_size;
-    int num_blocks_w = image_width - block_size;
+    unsigned int src_idx;
+    unsigned int out_idx;
+    int err_idx = 0;
 
-    int errors_size = num_blocks_h * num_blocks_w;
-    double *errors = malloc(errors_size * sizeof(double));
+    unsigned int max_src_x = src->width - block_size;
+    unsigned int max_src_y = src->height - block_size;
 
-    for (int y = 0; y < num_blocks_h; y++) {
-        for (int x = 0; x < num_blocks_w; x++) {
-            int y_start = y, x_start = x;
-            int y_end, x_end;
-            if (direction == ABOVE) {
-                y_end = y_start + overlap_size - 1;
-                x_end = x_start + block_size - 1;
-            } else if (direction == LEFT) {
-                y_end = y_start + block_size - 1;
-                x_end = x_start + overlap_size - 1;
-            } else if (direction == CORNER) {
-                y_end = y_start + overlap_size - 1;
-                x_end = x_start + overlap_size - 1;
-            }
+    double *errors = (double *) malloc(max_src_x * max_src_y * sizeof(double));
 
-            // Extract block from image
-            RGB *block_data = (RGB *) malloc(block_size * block_size * sizeof(RGB));
-            for (int by = y_start; by <= y_end; by++) {
-                for (int bx = x_start; bx <= x_end; bx++) {
-                    int idx = (by - y_start) * block_size + (bx - x_start);
-                    block_data[idx] = image->data[by * image_width + bx];
+
+    for (int candidate_y = 0; candidate_y < max_src_y; ++candidate_y) {
+        for (int candidate_x = 0; candidate_x < max_src_x; ++candidate_x) {
+
+
+            double error = 0;
+
+            if (direction == LEFT) {
+                // error on left overlap
+                for (int block_y = 0; block_y < block_size; ++block_y) {
+                    for (int block_x = 0; block_x < overlap; ++block_x) {
+                        src_idx = (block_y + candidate_y) * src->width + block_x + candidate_x;
+                        out_idx = (block_y + out_coord.y) * out->width + block_x + out_coord.x;
+
+                        error += rgb_sq_error(src->data + src_idx, out->data + out_idx);
+                    }
                 }
             }
-//            Image block = {block_size, block_size, block_data};
+            if (direction == ABOVE || direction == CORNER) {
+                // error on above overlap
+                for (int block_y = 0; block_y < overlap; ++block_y) {
+                    for (int block_x = 0; block_x < block_size; ++block_x) {
+                        src_idx = (block_y + candidate_y) * src->width + block_x + candidate_x;
+                        out_idx = (block_y + out_coord.y) * out->width + block_x + out_coord.x;
 
-            int error = 0;
-            for (int yy = 0; yy < y_end - y_start + 1; yy++) {
-                for (int xx = 0; xx < x_end - x_start + 1; xx++) {
-                    int idx = yy * block_size + xx;
-                    int pixel_diff[3] = {block_data[idx].r - out_slice->data[idx].r,
-                                         block_data[idx].g - out_slice->data[idx].g,
-                                         block_data[idx].b - out_slice->data[idx].b};
-                    error += pixel_diff[0] * pixel_diff[0] +
-                             pixel_diff[1] * pixel_diff[1] +
-                             pixel_diff[2] * pixel_diff[2];
+                        error += rgb_sq_error(src->data + src_idx, out->data + out_idx);
+                    }
                 }
             }
-            errors[y * num_blocks_w + x] = error;
+            if (direction == CORNER) {
+                // error left overlap without the section overlapping with the
+                for (int block_y = overlap; block_y < block_size; ++block_y) {
+                    for (int block_x = 0; block_x < overlap; ++block_x) {
+                        src_idx = (block_y + candidate_y) * src->width + block_x + candidate_x;
+                        out_idx = (block_y + out_coord.y) * out->width + block_x + out_coord.x;
 
-            free(block_data);
+                        error += rgb_sq_error(src->data + src_idx, out->data + out_idx);
+                    }
+                }
+            }
+
+            errors[err_idx] = error;
+            err_idx++;
         }
     }
+
     return errors;
 }
+
+/*
+ * chooses a random block from the source image with an error within the tolerance
+ */
+ImageCoordinates find_best_block(const double *errors, Image *src, int block_size) {
+    double tolerance = 1.3;
+    double min_err = DBL_MAX;
+    unsigned int max_src_x = src->width - block_size;
+    unsigned int max_src_y = src->height - block_size;
+
+    ImageCoordinates *candidate_coords = (ImageCoordinates *) malloc(max_src_x * max_src_y * sizeof(ImageCoordinates));
+    int candidate_idx = 0;
+
+    for (int i = 0; i < max_src_x * max_src_y; i++) {
+        double err = errors[i];
+        if (err < min_err) {
+            min_err = err;
+        }
+    }
+
+
+    double threshold = min_err * tolerance;
+
+    for (int y = 0; y < max_src_y; y++) {
+        for (int x = 0; x < max_src_x; x++) {
+            if (errors[y * max_src_x + x] <= threshold) {
+                ImageCoordinates coord = {x, y};
+                candidate_coords[candidate_idx] = coord;
+                candidate_idx++;
+            }
+        }
+    }
+
+    ImageCoordinates best_block_coord = candidate_coords[rand() % candidate_idx];
+    free(candidate_coords);
+    return best_block_coord;
+}
+
+
