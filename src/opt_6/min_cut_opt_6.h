78291e14f5fa761eda5a74242004a988ee27f478
Dominic <steinedo@student.ethz.ch>
2023-05-20 21:21:19 +0200

    vectorized cut mask merge
    cut mask now uses 2, 1, 0 instead of -1, 0, 1 to mark the cut path. This way we do not have to write all the zeros.

---
+++ src/opt_6/min_cut_opt_6.h
@@ -0,0 +1,21 @@
+#ifndef TEAM08_MIN_CUT_OPT_6_H
+#define TEAM08_MIN_CUT_OPT_6_H
+
+#include "src/matrix.h"
+#include "src/image.h"
+
+
+Matrix *calc_overlap_error_opt_6(
+        ImageRGB *source_image, ImageRGB *output_image, ImageCoordinates block_coords,
+        ImageCoordinates output_coords,
+        int overlap_width, int overlap_height
+);
+
+
+CutMatrix *min_cut_opt_6(
+        ImageRGB *source_image, ImageRGB *output_image, ImageCoordinates block_coords,
+        ImageCoordinates output_coords,
+        int block_size, int overlap, Direction direction
+);
+
+#endif //TEAM08_MIN_CUT_OPT_6_H
---
+++ src/opt_6/quilting_opt_6.h
@@ -0,0 +1,8 @@
+#ifndef QUILTING_OPT_6_H
+#define QUILTING_OPT_6_H
+
+#include "src/load_image.h"
+
+ImageRGB *quilting_opt_6(ImageRGB *image, int block_size, int out_num_blocks, int overlap_size);
+
+#endif //QUILTING_OPT_6_H
