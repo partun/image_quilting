994bc23bc5980b972d65311ad5585c50af5fdd58
Dominic <steinedo@student.ethz.ch>
2023-05-24 20:10:19 +0200

    different vectorization of calc error

---
+++ src/opt_6a/calc_errors_opt_6a.h
@@ -0,0 +1,23 @@
+//
+//
+//
+
+#ifndef CALC_ERRORS_OPT_6a_H
+#define CALC_ERRORS_OPT_6a_H
+
+#include "src/load_image.h"
+#include "src/matrix.h"
+
+extern int min_err;
+
+void calc_errors_opt_6a(
+        int *errors,
+        ImageRGB *src, ImageRGB *out, ImageCoordinates out_coord, int block_size,
+        int overlap, Direction direction
+);
+
+ImageCoordinates find_best_block_opt_6a(
+        const int *errors, ImageRGB *src, int block_size
+);
+
+#endif //CALC_ERRORS_OPT_6a_H
