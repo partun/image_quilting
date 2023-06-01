a3cf7baeab44c74e9a48b85ff51aba316e887aae
Dominic <steinedo@student.ethz.ch>
2023-06-01 18:38:55 +0200

    opt_9 added back optimized find_best_block from opt_6a

---
+++ src/opt_9/quilting_opt_9.h
@@ -0,0 +1,8 @@
+#ifndef QUILTING_OPT_9_H
+#define QUILTING_OPT_9_H
+
+#include "src/load_image.h"
+
+ImageRGB *quilting_opt_9(ImageRGB *image, int block_size, int out_num_blocks, int overlap_size);
+
+#endif //QUILTING_OPT_9_H
