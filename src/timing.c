82813d0b1f2bd94bbbeb506a09c2f4e0c392d165
emettaz <emettaz@student.ethz.ch>
2023-06-02 22:48:28 +0200

    Measurement of red, blue and green images.

--- src/timing.c
+++ src/timing.c
@@ -10,12 +10,15 @@
 #include "opt_6a/quilting_opt_6a.h"
 #include "opt_6b/quilting_opt_6b.h"
 #include "opt_6c/quilting_opt_6c.h"
+#include "opt_6d/quilting_opt_6d.h"
 #include "opt_8/quilting_opt_8.h"
+#include "opt_9/quilting_opt_9.h"
+#include "opt_10/quilting_opt_10.h"
 #include <stdio.h>
 #include "tsc_x86.h"
 #include "load_image.h"
 
-double CPU_FREQ_MHz = 3600;
+double CPU_FREQ_MHz = 1800;
 
 void time_quilt(
     Image *(*func)(Image *img, int block_size, int out_num_blocks, int overlap_size),
@@ -52,7 +55,7 @@ void multi_time_quilt(
                     free_image(quilt);
                 }
                 acc = acc / number_of_iter;
-                secs = acc / (CPU_FREQ_MHz * 1000000);
+                secs = (double) acc / (CPU_FREQ_MHz * 1000000);
                 fprintf(file, "%d,%d,%d,%llu,%f\n", i, j, k, acc, secs);
                 printf("\nblock: %d output: %d overlap: %d cycles: %llu runtime: %f\n", i, j, k,
                        acc, secs);
@@ -85,7 +88,7 @@ void multi_time_quilt_rgb(
                     free_image_rgb(quilt);
                 }
                 acc = acc / number_of_iter;
-                secs = acc / (CPU_FREQ_MHz * 1000000);
+                secs = (double) acc / (CPU_FREQ_MHz * 1000000);
                 fprintf(file, "%d,%d,%d,%llu,%f\n", i, j, k, acc, secs);
                 printf("\nblock: %d output: %d overlap: %d cycles: %llu runtime: %f\n", i, j, k,
                        acc, secs);
@@ -116,11 +119,11 @@ void time_fun(Image *img, FILE *file, Image *(*func)(Image *img, int block_size,
                 Image *quilt = func(img, i, out_num_blocks, k);
                 t = stop_tsc(timer);
                 acc += t;
-                printf("%f\n", (double) t / (CPU_FREQ_MHz * 1800000));
+                printf("%f\n", (double) t / (CPU_FREQ_MHz * 1000000));
                 free_image(quilt);
             }
             acc = acc / number_of_iter;
-            secs = (double) acc / (CPU_FREQ_MHz * 1800000);
+            secs = (double) acc / (CPU_FREQ_MHz * 1000000);
             fprintf(file, "%d,%d,%d,%llu,%f\n", i, out_num_blocks, k, acc, secs);
             printf("\nblock: %d output: %d overlap: %d cycles: %llu runtime: %f\n", i, out_num_blocks, k,
                    acc, secs);
@@ -151,11 +154,11 @@ void time_funRGB(ImageRGB *img, FILE *file, ImageRGB *(*func)(ImageRGB *img, int
                 ImageRGB *quilt = func(img, i, out_num_blocks, k);
                 t = stop_tsc(timer);
                 acc += t;
-                printf("%f\n", (double) t / (CPU_FREQ_MHz * 1800000));
+                printf("%f\n", (double) t / (CPU_FREQ_MHz * 100000));
                 free_image_rgb(quilt);
             }
             acc = acc / number_of_iter;
-            secs = (double) acc / (CPU_FREQ_MHz * 1800000);
+            secs = (double) acc / (CPU_FREQ_MHz * 1000000);
             fprintf(file, "%d,%d,%d,%llu,%f\n", i, out_num_blocks, k, acc, secs);
             printf("\nblock: %d output: %d overlap: %d cycles: %llu runtime: %f\n", i, out_num_blocks, k,
                    acc, secs);
@@ -164,38 +167,60 @@ void time_funRGB(ImageRGB *img, FILE *file, ImageRGB *(*func)(ImageRGB *img, int
     }
 }
 
+// 1. blue    2. red    3. green     start ~7:14
 void time_total() {
 
     FILE *file;
-        file = fopen("output/measure_3_8.txt", "w");
-        if (file == NULL) {
-            printf("Error opening file \n");
-            return;
-        }
+    file = fopen("output/measure_green.txt", "w");
+    if (file == NULL) {
+        printf("Error opening file \n");
+        return;
+    }
 
     fprintf(file, "block_size,number_of_blocks_in_output_image,overlap_size,number_of_cycles,"
                   "runtime_ms\n");
-    Image *img = read_image("../input/leaf.jpg");
     fprintf(file, "// leaf image\n");
+    char* input_path = "../input/leaf.jpg";
+
+    Image *img = read_image(input_path);
     fprintf(file, "// Baseline\n");
     time_fun(img, file, quilting_baseline);
+    free_image(img);
+
+    img = read_image(input_path);
     fprintf(file, "// Opt 4\n");
     time_fun(img, file, quilting_opt_4);
     free_image(img);
 
-    ImageRGB *img_rgb = read_image_rgb("../input/leaf.jpg");
+
+    ImageRGB *img_rgb = read_image_rgb(input_path);
     fprintf(file, "// Opt 5\n");
     time_funRGB(img_rgb, file, quilting_opt_5);
-    fprintf(file, "// Opt 6\n");
-    time_funRGB(img_rgb, file, quilting_opt_6);
+    free_image_rgb(img_rgb);
+
+    img_rgb = read_image_rgb(input_path);
     fprintf(file, "// Opt 6a\n");
     time_funRGB(img_rgb, file, quilting_opt_6a);
-    fprintf(file, "// Opt 6b\n");
-    time_funRGB(img_rgb, file, quilting_opt_6b);
+    free_image_rgb(img_rgb);
+
+    img_rgb = read_image_rgb(input_path);
     fprintf(file, "// Opt 6c\n");
     time_funRGB(img_rgb, file, quilting_opt_6c);
-    fprintf(file, "// Opt 8\n");
-    time_funRGB(img_rgb, file, quilting_opt_8);
+    free_image_rgb(img_rgb);
+
+    img_rgb = read_image_rgb(input_path);
+    fprintf(file, "// Opt 6d\n");
+    time_funRGB(img_rgb, file, quilting_opt_6d);
+    free_image_rgb(img_rgb);
+
+    img_rgb = read_image_rgb(input_path);
+    fprintf(file, "// Opt 9\n");
+    time_funRGB(img_rgb, file, quilting_opt_9);
+    free_image_rgb(img_rgb);
+
+    img_rgb = read_image_rgb(input_path);
+    fprintf(file, "// Opt 10\n");
+    time_funRGB(img_rgb, file, quilting_opt_10);
     free_image_rgb(img_rgb);
     fclose(file);
 
